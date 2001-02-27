f67b48c9937c5048bfa155bd94808ef6eb842ef6
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2001-02-27 10:48:47 +0000

    Seperate code and heap allocators.

--- trunk/joeq_native/joeq/Allocator/CodeAllocator.java
+++ trunk/joeq_native/joeq/Allocator/CodeAllocator.java
@@ -1,9 +1,9 @@
 /*
  * CodeAllocator.java
  *
- * Created on January 11, 2001, 10:55 AM
+ * Created on February 9, 2001, 8:40 AM
  *
- * @author  jwhaley
+ * @author  John Whaley
  * @version 
  */
 
@@ -11,59 +11,77 @@ package Allocator;
 
 import Clazz.jq_CompiledCode;
 import Clazz.jq_Method;
-import Clazz.jq_Class;
-import Clazz.jq_StaticField;
 import Clazz.jq_TryCatch;
 import Clazz.jq_BytecodeMap;
+import Clazz.jq_Class;
+import Clazz.jq_InstanceField;
+import Clazz.jq_StaticField;
 import Bootstrap.PrimordialClassLoader;
 import Run_Time.ExceptionDeliverer;
-import Run_Time.Unsafe;
-import jq;
 
-import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public abstract class CodeAllocator {
-
+    
     public static /*final*/ boolean TRACE = false;
     
-    private static final int LOWER_THRESHOLD = 0x00500000;
+    public static /*final*/ CodeAllocator DEFAULT;
     
-    private static SortedMap compiled_methods = new TreeMap();
+    public abstract void init();
+    public abstract x86CodeBuffer getCodeBuffer(int estimated_size);
+    public abstract void patchAbsolute(int/*CodeAddress*/ code, int/*HeapAddress*/ heap);
+    public abstract void patchRelativeOffset(int/*CodeAddress*/ code, int/*CodeAddress*/ target);
+
+    public abstract static class x86CodeBuffer {
+
+        public abstract int getCurrentOffset();
+        public abstract int/*CodeAddress*/ getCurrentAddress();
+        public abstract void add1(byte i);
+        public abstract void add2_endian(int i);
+        public abstract void add2(int i);
+        public abstract void add3(int i);
+        public abstract void add4_endian(int i);
+
+        public abstract byte get1(int k);
+        public abstract int get4_endian(int k);
+
+        public abstract void put1(int k, byte instr);
+        public abstract void put4_endian(int k, int instr);
+        
+        public abstract jq_CompiledCode allocateCodeBlock(jq_Method m, jq_TryCatch[] ex,
+                                                          jq_BytecodeMap bcm, ExceptionDeliverer exd,
+                                                          List code_relocs, List data_relocs);
+    }
+    
+    private static final SortedMap compiled_methods;
+    private static int/*CodeAddress*/ lowAddress = Integer.MAX_VALUE, highAddress = 0;
     static {
-        jq_CompiledCode cc = new jq_CompiledCode(null, 0, 0, null, null, null);
+        compiled_methods = new TreeMap();
+        jq_CompiledCode cc = new jq_CompiledCode(null, 0, 0, null, null, null, null, null);
         compiled_methods.put(cc, cc);
     }
     
-    public static jq_CompiledCode allocateCodeBlock(jq_Method m, x86CodeBuffer cb,
-                                                    jq_TryCatch[] ex, jq_BytecodeMap bcm,
-                                                    ExceptionDeliverer exd) {
-        int total = cb.getCurrentOffset();
-        byte[] instr = new byte[total];
-        int instr_ptr = 0;
-        for (int i=0; i<cb.bundle_idx; ++i) {
-            byte[] bundle = (byte[]) cb.bundles.elementAt(i);
-            System.arraycopy(bundle, 0, instr, instr_ptr, cb.bundle_size);
-            instr_ptr += cb.bundle_size;
-        }
-        System.arraycopy(cb.current_bundle, 0, instr, instr_ptr, cb.idx+1);
-        jq_CompiledCode cc = new jq_CompiledCode(m, Unsafe.addressOf(instr), total, ex, bcm, exd);
-        jq.assert(Unsafe.addressOf(instr) > LOWER_THRESHOLD, jq.hex8(Unsafe.addressOf(instr)));
+    public static void registerCode(jq_CompiledCode cc) {
+        if (TRACE) System.out.println("Registering code: "+cc);
+        lowAddress = Math.min(cc.getEntrypoint(), lowAddress);
+        highAddress = Math.max(cc.getEntrypoint()+cc.getLength(), highAddress);
         compiled_methods.put(cc, cc);
-        if (TRACE) System.out.println(cc.toString());
-        return cc;
     }
-    
-    public static int getStartAddress() { return LOWER_THRESHOLD; }
-    
-    public static jq_CompiledCode getCodeContaining(int ip) {
+    public static jq_CompiledCode getCodeContaining(int/*CodeAddress*/ ip) {
         return (jq_CompiledCode)compiled_methods.get(new InstructionPointer(ip));
     }
+    public static int/*CodeAddress*/ getLowAddress() { return lowAddress; }
+    public static int/*CodeAddress*/ getHighAddress() { return highAddress; }
     
     // NOTE: doesn't implement hashCode() very well!
     public static class InstructionPointer implements Comparable {
-        private final int/*Address*/ ip;
-        public InstructionPointer(int ip) { this.ip = ip; }
-        public int getIP() { return ip; }
+        private final int/*CodeAddress*/ ip;
+        public InstructionPointer(int/*CodeAddress*/ ip) { this.ip = ip; }
+        public int/*CodeAddress*/ getIP() { return ip; }
         public int compareTo(jq_CompiledCode that) {
             return -that.compareTo(this);
         }
@@ -91,92 +109,32 @@ public abstract class CodeAllocator {
                 return equals((InstructionPointer)that);
         }
         public int hashCode() { return 0; }
-    }
-    
-    public static class x86CodeBuffer {
-
-        public int getCurrentOffset() {
-            return bundle_size*bundle_idx+idx+1;
-        }
-
-        public void checkSize() {
-            if (idx == bundle_size-1) {
-                bundles.addElement(current_bundle = new byte[bundle_size]);
-                ++bundle_idx; idx=-1;
-            }
-        }
-
-        public void add1(byte i) {
-            checkSize(); current_bundle[++idx] = i;
-        }
-        public void add2_endian(int i) {
-            checkSize(); current_bundle[++idx] = (byte)(i);
-            checkSize(); current_bundle[++idx] = (byte)(i >> 8);
-        }
-        public void add2(int i) {
-            checkSize(); current_bundle[++idx] = (byte)(i >> 8);
-            checkSize(); current_bundle[++idx] = (byte)(i);
-        }
-        public void add3(int i) {
-            checkSize(); current_bundle[++idx] = (byte)(i >> 16);
-            checkSize(); current_bundle[++idx] = (byte)(i >> 8);
-            checkSize(); current_bundle[++idx] = (byte)(i);
-        }
-        public void add4_endian(int i) {
-            checkSize(); current_bundle[++idx] = (byte)(i);
-            checkSize(); current_bundle[++idx] = (byte)(i >> 8);
-            checkSize(); current_bundle[++idx] = (byte)(i >> 16);
-            checkSize(); current_bundle[++idx] = (byte)(i >> 24);
-        }
-
-        public byte get(int k) {
-            int i = k >> bundle_shift;
-            int j = k & bundle_mask;
-            byte[] b = (byte[])bundles.elementAt(i);
-            return b[j];
-        }
-
-        public int get4_endian (int k) {
-            int b = (int)get(k) & 0x000000FF;
-            b |= ((int)get(k+1) << 8) & 0x0000FF00;
-            b |= ((int)get(k+2) << 16) & 0x00FF0000;
-            b |= ((int)get(k+3) << 24) & 0xFF000000;
-            return b;
-        }
-
-        public void put1(int k, byte instr) {
-            int i = k >> bundle_shift;
-            int j = k & bundle_mask;
-            byte[] b = (byte[])bundles.elementAt(i);
-            b[j] = instr;
-        }
-
-        public void put4_endian(int k, int instr) {
-            put1(k, (byte)(instr));
-            put1(k+1, (byte)(instr>>8));
-            put1(k+2, (byte)(instr>>16));
-            put1(k+3, (byte)(instr>>24));
-        }
-
-        private static final int bundle_shift = 12;
-        private static final int bundle_size = 1 << bundle_shift;
-        private static final int bundle_mask = bundle_size-1;
-        private Vector bundles;
-        private byte[] current_bundle;
-        private int bundle_idx;
-        private int idx;
-
-        public x86CodeBuffer() {
-            bundles = new Vector();
-            bundles.addElement(current_bundle = new byte[bundle_size]);
-            bundle_idx = 0;
-            idx = -1;
+        
+        public static final jq_InstanceField _ip;
+        static {
+            jq_Class k = (jq_Class)PrimordialClassLoader.loader.getOrCreateBSType("LAllocator/CodeAllocator$InstructionPointer;");
+            _ip = k.getOrCreateInstanceField("ip", "I");
         }
     }
     
+    public static final Set codeAddressFields;
     public static final jq_StaticField _compiled_methods;
+    public static final jq_StaticField _DEFAULT;
+    public static final jq_StaticField _lowAddress;
+    public static final jq_StaticField _highAddress;
     static {
         jq_Class k = (jq_Class)PrimordialClassLoader.loader.getOrCreateBSType("LAllocator/CodeAllocator;");
-        _compiled_methods = k.getOrCreateStaticField("compiled_methods", "Ljava/util/SortedSet;");
+        _compiled_methods = k.getOrCreateStaticField("compiled_methods", "Ljava/util/SortedMap;");
+        _DEFAULT = k.getOrCreateStaticField("DEFAULT", "LAllocator/CodeAllocator;");
+        _lowAddress = k.getOrCreateStaticField("lowAddress", "I");
+        _highAddress = k.getOrCreateStaticField("highAddress", "I");
+        codeAddressFields = new HashSet();
+        codeAddressFields.add(Assembler.x86.Code2HeapReference._from_codeloc);
+        codeAddressFields.add(Assembler.x86.Heap2CodeReference._to_codeloc);
+        codeAddressFields.add(Assembler.x86.DirectBindCall._source);
+        codeAddressFields.add(Clazz.jq_CompiledCode._entrypoint);
+        codeAddressFields.add(InstructionPointer._ip);
+        codeAddressFields.add(_lowAddress);
+        codeAddressFields.add(_highAddress);
     }
 }
