7e349a58e6b1ac51b5cd4ac9ba3fe9da46d33a1a
joewhaley <joewhaley@users.noreply.github.com>
2001-04-09 08:02:47 +0000

    Support for thin locks.

--- joeq_core/joeq/Allocator/ObjectLayout.java
+++ joeq_core/joeq/Allocator/ObjectLayout.java
@@ -21,4 +21,12 @@ public interface ObjectLayout {
 
     public static final int HASHED       = 0x00000001;
     public static final int HASHED_MOVED = 0x00000002;
+    
+    public static final int STATUS_FLAGS_MASK = 0x00000007;
+
+    public static final int THREAD_ID_SHIFT   = 9;
+    public static final int THREAD_ID_MASK    = 0x7FFFFE00;
+    public static final int LOCK_COUNT_MASK   = 0x000001F0;
+    public static final int LOCK_COUNT_INC    = 0x00000010;
+    public static final int LOCK_EXPANDED     = 0x80000000;
 }
