f18d60d5e48ae0f21da3c599ddc9023a74a4519d
joewhaley <joewhaley@users.noreply.github.com>
2001-04-09 08:30:26 +0000

    *** empty log message ***

---
+++ joeq_core/joeq/Class/jq_DontAlign.java
@@ -0,0 +1,17 @@
+/*
+ * jq_DontAlign.java
+ *
+ * Created on April 3, 2001, 8:29 PM
+ *
+ * @author  John Whaley
+ * @version 
+ */
+
+package Clazz;
+
+import Bootstrap.PrimordialClassLoader;
+
+public interface jq_DontAlign {
+
+    public static final jq_Class _class = (jq_Class)PrimordialClassLoader.loader.getOrCreateBSType("LClazz/jq_DontAlign;");
+}
