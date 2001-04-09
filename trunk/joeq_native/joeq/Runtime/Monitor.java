05e27da4553eda25989d4c1cd162768fb88efd57
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2001-04-09 08:46:23 +0000

    Added thin (and fat) lock support.

--- trunk/joeq_native/joeq/Runtime/Monitor.java
+++ trunk/joeq_native/joeq/Runtime/Monitor.java
@@ -9,25 +9,248 @@
 
 package Run_Time;
 
+import Allocator.DefaultHeapAllocator;
+import Allocator.ObjectLayout;
 import Bootstrap.PrimordialClassLoader;
 import Clazz.jq_Class;
 import Clazz.jq_StaticMethod;
+import Clazz.jq_InstanceField;
+import Run_Time.Unsafe;
+import Scheduler.jq_Thread;
+import jq;
 
-public abstract class Monitor {
+public class Monitor implements ObjectLayout {
 
+    public static /*final*/ boolean TRACE = false;
+    
+    private Monitor() {}
+    
+    int atomic_count = 0;  // -1 means no threads; 0 means one thread (monitor_owner)
+    jq_Thread monitor_owner;
+    int entry_count = 0;    // 1 means locked once.
+    int/*CPointer*/ semaphore;
+  
+    /** Monitorenter runtime routine.
+     *  Checks for thin lock usage, otherwise falls back to inflated locks.
+     */
     public static void monitorenter(Object k) {
-        // todo
-    }
-    public static void monitorexit(Object k) {
-        // todo
+        jq_Thread t = Unsafe.getThreadBlock();
+        int tid = t.getThreadId(); // pre-shifted thread id
+        
+        // attempt fast path: object is not locked.
+        int status_flags = Unsafe.peek(Unsafe.addressOf(k)+STATUS_WORD_OFFSET) & STATUS_FLAGS_MASK;
+        int newlockword = status_flags | tid;
+        int oldlockword = Unsafe.atomicCas4(Unsafe.addressOf(k)+STATUS_WORD_OFFSET, status_flags, newlockword);
+        if (Unsafe.isEQ()) {
+            // fast path: not locked
+            return;
+        }
+        
+        // object is locked or has an inflated lock.
+        int counter = oldlockword ^ newlockword; // if tid's are equal, this extracts the counter.
+        if (counter >= LOCK_COUNT_MASK) {
+            // slow path: other thread owns thin lock, or entry counter == max
+            int entrycount;
+            if (counter == LOCK_COUNT_MASK) {
+                // thin lock entry counter == max, so we need to inflate ourselves.
+                if (TRACE) SystemInterface.debugmsg("Thin lock counter overflow, inflating lock...");
+                entrycount = (LOCK_COUNT_MASK >> LOCK_COUNT_SHIFT)+2;
+                Monitor m = allocateInflatedLock();
+                m.monitor_owner = t;
+                m.entry_count = entrycount;
+                newlockword = Unsafe.addressOf(m) | LOCK_EXPANDED | status_flags;
+                // we own the lock, so a simple write is sufficient.
+                jq.assert(Unsafe.peek(Unsafe.addressOf(k)+STATUS_WORD_OFFSET) == oldlockword);
+                Unsafe.poke4(Unsafe.addressOf(k)+STATUS_WORD_OFFSET, newlockword);
+            } else {
+                // thin lock owned by another thread.
+                if (TRACE) SystemInterface.debugmsg("Lock contention ("+t+"), inflating...");
+                entrycount = 1;
+                Monitor m = allocateInflatedLock();
+                m.monitor_owner = t;
+                m.entry_count = entrycount;
+                // install an inflated lock.
+                installInflatedLock(k, m);
+            }
+        } else if (counter < 0) {
+            // slow path 2: high bit of lock word is set --> inflated lock
+            Monitor m = getMonitor(oldlockword);
+            m.lock(t);
+        } else {
+            // not-quite-so-fast path: locked by current thread.  increment counter.
+            // we own the lock, so a simple write is sufficient.
+            Unsafe.poke4(Unsafe.addressOf(k)+STATUS_WORD_OFFSET, oldlockword+LOCK_COUNT_INC);
+        }
     }
     
+    /** Monitorexit runtime routine.
+     *  Checks for thin lock usage, otherwise falls back to inflated locks.
+     */
+    public static void monitorexit(Object k) {
+        jq_Thread t = Unsafe.getThreadBlock();
+        int tid = t.getThreadId(); // pre-shifted
+        int oldlockword = Unsafe.peek(Unsafe.addressOf(k)+STATUS_WORD_OFFSET);
+        // if not inflated and tid matches, this contains status flags and counter
+        int counter = oldlockword ^ tid;
+        if (counter < 0) {
+            // inflated lock
+            Monitor m = getMonitor(oldlockword);
+            m.unlock(t);
+        } else if (counter <= STATUS_FLAGS_MASK) {
+            // owned by us and count is zero.  clear tid field.
+            Unsafe.atomicAnd(Unsafe.addressOf(k)+STATUS_WORD_OFFSET, STATUS_FLAGS_MASK);
+        } else if (counter <= (LOCK_COUNT_MASK | STATUS_FLAGS_MASK)) {
+            // owned by us but count is non-zero.  decrement count.
+            Unsafe.atomicSub(Unsafe.addressOf(k)+STATUS_WORD_OFFSET, LOCK_COUNT_INC);
+        } else {
+            // lock not owned by us!
+            //if (TRACE)
+                SystemInterface.debugmsg("Thin lock not owned by us ("+jq.hex8(tid)+")! lockword="+jq.hex8(oldlockword));
+            throw new IllegalMonitorStateException();
+        }
+    }
+    
+    /** Get the Monitor object associated with this lockword. */
+    public static Monitor getMonitor(int lockword) {
+        return (Monitor)Unsafe.asObject(lockword & (~LOCK_EXPANDED & ~STATUS_FLAGS_MASK));
+    }
+    
+    public static Monitor allocateInflatedLock() {
+        // Monitor must be 8-byte aligned.
+        return (Monitor)DefaultHeapAllocator.allocateObjectAlign8(_class.getInstanceSize(), _class.getVTable());
+    }
+    
+    public void free() {
+        // nop. we will be garbage collected.
+    }
+    
+    /** Installs an inflated lock on the given object.
+     *  Uses a spin-loop to wait until the object is unlocked or inflated.
+     */
+    public static void installInflatedLock(Object k, Monitor m) {
+        jq.assert(m.monitor_owner == Unsafe.getThreadBlock());
+        jq.assert(m.entry_count >= 1);
+        for (;;) {
+            int oldlockword = Unsafe.peek(Unsafe.addressOf(k)+STATUS_WORD_OFFSET);
+            if (oldlockword < 0) {
+                // inflated by another thread!  free our inflated lock and use that one.
+                jq.assert(m.entry_count == 1);
+                m.free();
+                Monitor m2 = getMonitor(oldlockword);
+                if (TRACE) SystemInterface.debugmsg("Inflated by another thread! lockword="+jq.hex8(oldlockword)+" lock="+m2);
+                jq.assert(m != m2);
+                m2.lock(Unsafe.getThreadBlock());
+                return;
+            }
+            int status_flags = oldlockword & STATUS_FLAGS_MASK;
+            if ((Unsafe.addressOf(m) & STATUS_FLAGS_MASK) != 0) {
+                jq.UNREACHABLE("Monitor object has address "+jq.hex8(Unsafe.addressOf(m)));
+            }
+            int newlockword = Unsafe.addressOf(m) | LOCK_EXPANDED | status_flags;
+            Unsafe.atomicCas4(Unsafe.addressOf(k)+STATUS_WORD_OFFSET, status_flags, newlockword);
+            if (Unsafe.isEQ()) {
+                // successfully obtained inflated lock.
+                if (TRACE) SystemInterface.debugmsg("Obtained inflated lock! new lockword="+jq.hex8(newlockword));
+                return;
+            }
+            // another thread has a thin lock on this object.  yield to scheduler.
+            Thread.yield();
+        }
+    }
+
+    /** Lock this monitor with the given thread block.
+     */
+    public void lock(jq_Thread t) {
+        jq_Thread m_t = this.monitor_owner;
+        if (m_t == t) {
+            // we own the lock.
+            jq.assert(this.atomic_count >= 0);
+            jq.assert(this.entry_count > 0);
+            ++this.entry_count;
+            if (TRACE) SystemInterface.debugmsg("We ("+t+") own lock "+this+", incrementing entry count: "+this.entry_count);
+            return;
+        }
+        // another thread or no thread owns the lock. increase atomic count.
+        Unsafe.atomicAdd(Unsafe.addressOf(this)+_atomic_count.getOffset(), 1);
+        if (!Unsafe.isEQ()) {
+            // someone else already owns the lock.
+            if (TRACE) SystemInterface.debugmsg("Someone else ("+m_t+") owns lock "+this+", waiting on semaphore ("+this.atomic_count+" waiters)");
+            // create a semaphore if there isn't one already, and wait on it.
+            this.waitOnSemaphore();
+            if (TRACE) SystemInterface.debugmsg("We ("+t+") finished waiting on "+this);
+        } else {
+            if (TRACE) SystemInterface.debugmsg(this+" is unlocked, we ("+t+") obtain it.");
+        }
+        jq.assert(this.monitor_owner == null);
+        jq.assert(this.entry_count == 0);
+        if (TRACE) SystemInterface.debugmsg("We ("+t+") obtained lock "+this);
+        this.monitor_owner = t;
+        this.entry_count = 1;
+    }
+    
+    /** Unlock this monitor with the given thread block.
+     */
+    public void unlock(jq_Thread t) {
+        jq_Thread m_t = this.monitor_owner;
+        if (m_t != t) {
+            // lock not owned by us!
+            //if (TRACE)
+                SystemInterface.debugmsg("We ("+t+") tried to unlock lock "+this+" owned by "+m_t);
+            throw new IllegalMonitorStateException();
+        }
+        if (--this.entry_count > 0) {
+            // not zero yet.
+            if (TRACE) SystemInterface.debugmsg("Decrementing lock "+this+" entry count "+this.entry_count);
+            return;
+        }
+        this.monitor_owner = null;
+        Unsafe.atomicSub(Unsafe.addressOf(this)+_atomic_count.getOffset(), 1);
+        if (this.atomic_count >= 0) {
+            // threads are waiting on us, release the semaphore.
+            if (TRACE) SystemInterface.debugmsg(this.atomic_count+" threads are waiting on released lock "+this+", releasing semaphore.");
+            this.releaseSemaphore();
+        } else {
+            if (TRACE) SystemInterface.debugmsg("No threads are waiting on released lock "+this+".");
+        }
+    }
+    
+    /** Create a semaphore if there isn't one already, and wait on it.
+     */
+    public void waitOnSemaphore() {
+        if (this.semaphore == 0) {
+            this.semaphore = SystemInterface.init_semaphore();
+        }
+        // TODO: integrate waiting on a semaphore into the scheduler
+        for (;;) {
+            int rc = SystemInterface.wait_for_single_object(this.semaphore, 10); // timeout
+            if (rc == SystemInterface.WAIT_TIMEOUT) {
+                Thread.yield();
+                continue;
+            } else if (rc == 0) {
+                return;
+            } else {
+                SystemInterface.debugmsg("Bad return value from WaitForSingleObject: "+rc);
+            }
+        }
+    }
+    /** Create a semaphore if there isn't one already, and release it.
+     */
+    public void releaseSemaphore() {
+        if (this.semaphore == 0) {
+            this.semaphore = SystemInterface.init_semaphore();
+        }
+        int rc = SystemInterface.release_semaphore(this.semaphore, 1);
+    }
+    
+    public static final jq_Class _class;
     public static final jq_StaticMethod _monitorenter;
     public static final jq_StaticMethod _monitorexit;
+    public static final jq_InstanceField _atomic_count;
     static {
-        jq_Class k = (jq_Class)PrimordialClassLoader.loader.getOrCreateBSType("LRun_Time/Monitor;");
-        _monitorenter = k.getOrCreateStaticMethod("monitorenter", "(Ljava/lang/Object;)V");
-        _monitorexit = k.getOrCreateStaticMethod("monitorexit", "(Ljava/lang/Object;)V");
+        _class = (jq_Class)PrimordialClassLoader.loader.getOrCreateBSType("LRun_Time/Monitor;");
+        _monitorenter = _class.getOrCreateStaticMethod("monitorenter", "(Ljava/lang/Object;)V");
+        _monitorexit = _class.getOrCreateStaticMethod("monitorexit", "(Ljava/lang/Object;)V");
+        _atomic_count = _class.getOrCreateInstanceField("atomic_count", "I");
     }
 
 }
