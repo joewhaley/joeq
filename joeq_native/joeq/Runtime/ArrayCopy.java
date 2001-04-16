3ba84e709fce1e50b5f5558e16836f27be0d181a
joewhaley <joewhaley@users.noreply.github.com>
2001-04-16 23:09:52 +0000

    Added informative message to ArrayStoreExceptions when arraycopy is passed invalid arguments. Added correct bounds checking, so bounds errors are thrown BEFORE the arrays are modified. Fixed the case with overlapping arrays.

--- joeq_native/joeq/Runtime/ArrayCopy.java
+++ joeq_native/joeq/Runtime/ArrayCopy.java
@@ -14,54 +14,57 @@ public abstract class ArrayCopy {
     public static void arraycopy(Object src, int src_position,
                                  Object dst, int dst_position,
                                  int length) {
-        if (dst instanceof Object[])
-            if (src instanceof Object[])
+        if (dst instanceof Object[]) {
+            if (src instanceof Object[]) {
                 ArrayCopy.arraycopy((Object[])src, src_position, (Object[])dst, dst_position, length);
-            else
-                throw new ArrayStoreException();
-        else if (dst instanceof byte[])
-            if (src instanceof byte[])
+                return;
+            }
+        } else if (dst instanceof byte[]) {
+            if (src instanceof byte[]) {
                 ArrayCopy.arraycopy((byte[])src, src_position, (byte[])dst, dst_position, length);
-            else
-                throw new ArrayStoreException();
-        else if (dst instanceof char[])
-            if (src instanceof char[])
+                return;
+            }
+        } else if (dst instanceof char[]) {
+            if (src instanceof char[]) {
                 ArrayCopy.arraycopy((char[])src, src_position, (char[])dst, dst_position, length);
-            else
-                throw new ArrayStoreException();
-        else if (dst instanceof short[])
-            if (src instanceof short[])
+                return;
+            }
+        } else if (dst instanceof short[]) {
+            if (src instanceof short[]) {
                 ArrayCopy.arraycopy((short[])src, src_position, (short[])dst, dst_position, length);
-            else
-                throw new ArrayStoreException();
-        else if (dst instanceof int[])
-            if (src instanceof int[])
+                return;
+            }
+        } else if (dst instanceof int[]) {
+            if (src instanceof int[]) {
                 ArrayCopy.arraycopy((int[])src, src_position, (int[])dst, dst_position, length);
-            else
-                throw new ArrayStoreException();
-        else if (dst instanceof float[])
-            if (src instanceof float[])
+                return;
+            }
+        } else if (dst instanceof float[]) {
+            if (src instanceof float[]) {
                 ArrayCopy.arraycopy((float[])src, src_position, (float[])dst, dst_position, length);
-            else
-                throw new ArrayStoreException();
-        else if (dst instanceof long[])
-            if (src instanceof long[])
+                return;
+            }
+        } else if (dst instanceof long[]) {
+            if (src instanceof long[]) {
                 ArrayCopy.arraycopy((long[])src, src_position, (long[])dst, dst_position, length);
-            else
-                throw new ArrayStoreException();
-        else if (dst instanceof double[])
-            if (src instanceof double[])
+                return;
+            }
+        } else if (dst instanceof double[]) {
+            if (src instanceof double[]) {
                 ArrayCopy.arraycopy((double[])src, src_position, (double[])dst, dst_position, length);
-            else
-                throw new ArrayStoreException();
-        else
-            throw new ArrayStoreException();
+                return;
+            }
+        }
+        throw new ArrayStoreException("destination array type "+dst.getClass()+" does not match source array type "+src.getClass());
     }
     
     public static void arraycopy(Object[] src, int src_position,
                                  Object[] dst, int dst_position,
                                  int length) {
-        if ((src == dst) && (dst_position < src_position) && (dst_position+length > src_position)) {
+        if ((src_position < 0) || (dst_position < 0) || (length < 0) ||
+            (src_position+length > src.length) || (dst_position+length > dst.length))
+            throw new ArrayIndexOutOfBoundsException();
+        if ((src == dst) && (src_position < dst_position) && (src_position+length > dst_position)) {
             // overlapping case
             for (int i=length-1; i>=0; --i) {
                 dst[dst_position+i] = src[src_position+i];
@@ -75,7 +78,10 @@ public abstract class ArrayCopy {
     public static void arraycopy(byte[] src, int src_position,
                                  byte[] dst, int dst_position,
                                  int length) {
-        if ((src == dst) && (dst_position < src_position) && (dst_position+length > src_position)) {
+        if ((src_position < 0) || (dst_position < 0) || (length < 0) ||
+            (src_position+length > src.length) || (dst_position+length > dst.length))
+            throw new ArrayIndexOutOfBoundsException();
+        if ((src == dst) && (src_position < dst_position) && (src_position+length > dst_position)) {
             // overlapping case
             for (int i=length-1; i>=0; --i) {
                 dst[dst_position+i] = src[src_position+i];
@@ -89,7 +95,10 @@ public abstract class ArrayCopy {
     public static void arraycopy(char[] src, int src_position,
                                  char[] dst, int dst_position,
                                  int length) {
-        if ((src == dst) && (dst_position < src_position) && (dst_position+length > src_position)) {
+        if ((src_position < 0) || (dst_position < 0) || (length < 0) ||
+            (src_position+length > src.length) || (dst_position+length > dst.length))
+            throw new ArrayIndexOutOfBoundsException();
+        if ((src == dst) && (src_position < dst_position) && (src_position+length > dst_position)) {
             // overlapping case
             for (int i=length-1; i>=0; --i) {
                 dst[dst_position+i] = src[src_position+i];
@@ -103,7 +112,10 @@ public abstract class ArrayCopy {
     public static void arraycopy(short[] src, int src_position,
                                  short[] dst, int dst_position,
                                  int length) {
-        if ((src == dst) && (dst_position < src_position) && (dst_position+length > src_position)) {
+        if ((src_position < 0) || (dst_position < 0) || (length < 0) ||
+            (src_position+length > src.length) || (dst_position+length > dst.length))
+            throw new ArrayIndexOutOfBoundsException();
+        if ((src == dst) && (src_position < dst_position) && (src_position+length > dst_position)) {
             // overlapping case
             for (int i=length-1; i>=0; --i) {
                 dst[dst_position+i] = src[src_position+i];
@@ -117,7 +129,10 @@ public abstract class ArrayCopy {
     public static void arraycopy(int[] src, int src_position,
                                  int[] dst, int dst_position,
                                  int length) {
-        if ((src == dst) && (dst_position < src_position) && (dst_position+length > src_position)) {
+        if ((src_position < 0) || (dst_position < 0) || (length < 0) ||
+            (src_position+length > src.length) || (dst_position+length > dst.length))
+            throw new ArrayIndexOutOfBoundsException();
+        if ((src == dst) && (src_position < dst_position) && (src_position+length > dst_position)) {
             // overlapping case
             for (int i=length-1; i>=0; --i) {
                 dst[dst_position+i] = src[src_position+i];
@@ -131,7 +146,10 @@ public abstract class ArrayCopy {
     public static void arraycopy(float[] src, int src_position,
                                  float[] dst, int dst_position,
                                  int length) {
-        if ((src == dst) && (dst_position < src_position) && (dst_position+length > src_position)) {
+        if ((src_position < 0) || (dst_position < 0) || (length < 0) ||
+            (src_position+length > src.length) || (dst_position+length > dst.length))
+            throw new ArrayIndexOutOfBoundsException();
+        if ((src == dst) && (src_position < dst_position) && (src_position+length > dst_position)) {
             // overlapping case
             for (int i=length-1; i>=0; --i) {
                 dst[dst_position+i] = src[src_position+i];
@@ -145,7 +163,10 @@ public abstract class ArrayCopy {
     public static void arraycopy(long[] src, int src_position,
                                  long[] dst, int dst_position,
                                  int length) {
-        if ((src == dst) && (dst_position < src_position) && (dst_position+length > src_position)) {
+        if ((src_position < 0) || (dst_position < 0) || (length < 0) ||
+            (src_position+length > src.length) || (dst_position+length > dst.length))
+            throw new ArrayIndexOutOfBoundsException();
+        if ((src == dst) && (src_position < dst_position) && (src_position+length > dst_position)) {
             // overlapping case
             for (int i=length-1; i>=0; --i) {
                 dst[dst_position+i] = src[src_position+i];
@@ -159,7 +180,10 @@ public abstract class ArrayCopy {
     public static void arraycopy(double[] src, int src_position,
                                  double[] dst, int dst_position,
                                  int length) {
-        if ((src == dst) && (dst_position < src_position) && (dst_position+length > src_position)) {
+        if ((src_position < 0) || (dst_position < 0) || (length < 0) ||
+            (src_position+length > src.length) || (dst_position+length > dst.length))
+            throw new ArrayIndexOutOfBoundsException();
+        if ((src == dst) && (src_position < dst_position) && (src_position+length > dst_position)) {
             // overlapping case
             for (int i=length-1; i>=0; --i) {
                 dst[dst_position+i] = src[src_position+i];
