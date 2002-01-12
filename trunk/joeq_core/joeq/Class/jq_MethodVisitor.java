8401f10eab5ecf30d01d0280343d6e7a8a08c916
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-01-12 01:28:36 +0000

    Major check in to prepare for CS243 class.

---
+++ trunk/joeq_core/joeq/Class/jq_MethodVisitor.java
@@ -0,0 +1,46 @@
+/*
+ * jq_MethodVisitor.java
+ *
+ * Created on January 9, 2002, 9:40 AM
+ *
+ * @author  Administrator
+ * @version 
+ */
+
+package Clazz;
+
+import Util.ArrayIterator;
+import Util.AppendIterator;
+import java.util.Iterator;
+
+public interface jq_MethodVisitor {
+
+    public void visitClassInitializer(jq_ClassInitializer m);
+    public void visitInitializer(jq_Initializer m);
+    public void visitStaticMethod(jq_StaticMethod m);
+    public void visitInstanceMethod(jq_InstanceMethod m);
+    public void visitMethod(jq_Method m);
+    
+    public class EmptyVisitor implements jq_MethodVisitor {
+        public void visitClassInitializer(jq_ClassInitializer m) {}
+        public void visitInitializer(jq_Initializer m) {}
+        public void visitStaticMethod(jq_StaticMethod m) {}
+        public void visitInstanceMethod(jq_InstanceMethod m) {}
+        public void visitMethod(jq_Method m) {}
+    }
+    
+    public class DeclaredMethodVisitor extends jq_TypeVisitor.EmptyVisitor {
+        final jq_MethodVisitor mv;
+        public DeclaredMethodVisitor(jq_MethodVisitor mv) { this.mv = mv; }
+        public void visitClass(jq_Class k) {
+            Iterator it = new AppendIterator(new ArrayIterator(k.getDeclaredStaticMethods()),
+                                                new ArrayIterator(k.getDeclaredInstanceMethods()));
+            while (it.hasNext()) {
+                jq_Method m = (jq_Method)it.next();
+                m.accept(mv);
+            }
+        }
+    }
+    
+}
+
