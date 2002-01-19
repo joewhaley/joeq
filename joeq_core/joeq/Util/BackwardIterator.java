493a859eb6cd415df3e8d7671908a67cccc5b792
joewhaley <joewhaley@users.noreply.github.com>
2002-01-19 03:34:36 +0000

    *** empty log message ***

---
+++ joeq_core/joeq/Util/BackwardIterator.java
@@ -0,0 +1,37 @@
+/*
+ * BackwardIterator.java
+ *
+ * Created on January 17, 2002, 5:42 PM
+ */
+
+package Util;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ *
+ * @author  Administrator
+ * @version 
+ */
+public class BackwardIterator implements ListIterator {
+
+    private final ListIterator i;
+    public BackwardIterator(ListIterator x) {
+        while (x.hasNext()) x.next();
+        this.i = x;
+    }
+    public BackwardIterator(List x) {
+        this.i = x.listIterator(x.size());
+    }
+    
+    public int previousIndex() { return i.nextIndex(); }
+    public boolean hasNext() { return i.hasPrevious(); }
+    public void set(Object obj) { i.set(obj); }
+    public Object next() { return i.previous(); }
+    public int nextIndex() { return i.previousIndex(); }
+    public void remove() { i.remove(); }
+    public boolean hasPrevious() { return i.hasNext(); }
+    public void add(Object obj) { i.add(obj); i.previous(); }
+    public Object previous() { return i.next(); }
+    
+}
