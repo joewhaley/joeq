4a7fa5c09f1aba19a3ebcfbad0ea8fa5210ca677
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-02-06 00:57:30 +0000

    Added quad numbering for printout purposes.

--- trunk/joeq_core/joeq/Compiler/Quad/Quad.java
+++ trunk/joeq_core/joeq/Compiler/Quad/Quad.java
@@ -18,33 +18,53 @@ import jq;
 
 public class Quad {
 
+    /** The operator.  Operator objects are shared across all quads. */
     private Operator operator;
+    /** The four operands.  Operands are quad-specific. */
     private Operand operand1, operand2, operand3, operand4;
+    /** Id number for this quad.  THIS NUMBER HOLDS NO MEANING WHATSOEVER.  It is just used for printing. */
+    private int id_number;
     
     /** Creates new Quad */
-    Quad(Operator operator) {
-        this.operator = operator;
+    Quad(int id, Operator operator) {
+        this.id_number = id; this.operator = operator;
     }
-    Quad(Operator operator, Operand operand1) {
-        this.operator = operator; this.operand1 = operand1;
+    Quad(int id, Operator operator, Operand operand1) {
+        this.id_number = id; this.operator = operator; this.operand1 = operand1;
     }
-    Quad(Operator operator, Operand operand1, Operand operand2) {
-        this.operator = operator; this.operand1 = operand1; this.operand2 = operand2;
+    Quad(int id, Operator operator, Operand operand1, Operand operand2) {
+        this.id_number = id; this.operator = operator; this.operand1 = operand1; this.operand2 = operand2;
     }
-    Quad(Operator operator, Operand operand1, Operand operand2, Operand operand3) {
-        this.operator = operator; this.operand1 = operand1; this.operand2 = operand2; this.operand3 = operand3;
+    Quad(int id, Operator operator, Operand operand1, Operand operand2, Operand operand3) {
+        this.id_number = id; this.operator = operator; this.operand1 = operand1; this.operand2 = operand2; this.operand3 = operand3;
     }
-    Quad(Operator operator, Operand operand1, Operand operand2, Operand operand3, Operand operand4) {
-        this.operator = operator; this.operand1 = operand1; this.operand2 = operand2; this.operand3 = operand3; this.operand4 = operand4;
+    Quad(int id, Operator operator, Operand operand1, Operand operand2, Operand operand3, Operand operand4) {
+        this.id_number = id; this.operator = operator; this.operand1 = operand1; this.operand2 = operand2; this.operand3 = operand3; this.operand4 = operand4;
     }
+    /** These are not intended to be used outside of the Compil3r.Quad package.
+     * Instead, use the static accessor methods for each operator, e.g. Move.getDest(quad).
+     */
     Operand getOp1() { return operand1; }
     Operand getOp2() { return operand2; }
     Operand getOp3() { return operand3; }
     Operand getOp4() { return operand4; }
+    void setOp1(Operand op) { operand1 = op; }
+    void setOp2(Operand op) { operand2 = op; }
+    void setOp3(Operand op) { operand3 = op; }
+    void setOp4(Operand op) { operand4 = op; }
+    
+    /** Return the operator for this quad. */
     public Operator getOperator() { return operator; }
     
-    void accept(QuadVisitor qv) { this.operator.accept(this, qv); }
+    /** Accepts a quad visitor to this quad.  For the visitor pattern. */
+    public void accept(QuadVisitor qv) { this.operator.accept(this, qv); }
+
+    /** Returns the id number of this quad.  THIS NUMBER HOLDS NO MEANING WHATSOEVER.  It is just used for printing. */
+    public int getID() { return id_number; }
     
+    /** Returns a list of the types of exceptions that this quad can throw.
+     * Note that types in this list are not exact, therefore subtypes of the
+     * returned types may also be thrown. */
     public List.jq_Class getThrownExceptions() {
 	if (operator == Return.THROW_A.INSTANCE) {
 	    Operand op = Return.getSrc(this);
@@ -56,31 +76,27 @@ public class Quad {
 	return this.operator.getThrownExceptions();
     }
 
+    /** Returns a list of the registers defined by this quad. */
     public List.RegisterOperand getDefinedRegisters() { return this.operator.getDefinedRegisters(this); }
+    /** Returns a list of the registers used by this quad. */
     public List.RegisterOperand getUsedRegisters() { return this.operator.getUsedRegisters(this); }
     
+    /** Returns a string representation of this quad. */
     public String toString() {
         StringBuffer s = new StringBuffer();
+        s.append(jq.left(Integer.toString(id_number), 4));
         s.append(jq.left(operator.toString(), 30));
-        if (operand1 != null) {
-            s.append(operand1.toString());
-        } else
-            s.append("      ");
-        if (operand2 != null) {
-	    if (operand1 != null) s.append(',');
-	    s.append('\t');
-            s.append(operand2.toString());
-        } else
-            return s.toString();
-        if (operand3 != null) {
-	    s.append(",\t");
-            s.append(operand3.toString());
-        } else
-            return s.toString();
-        if (operand4 != null) {
-	    s.append(",\t");
-            s.append(operand4.toString());
-	}
+        if (operand1 == null) return s.toString();
+        s.append(operand1.toString());
+        if (operand2 == null) return s.toString();
+        s.append(",\t");
+        s.append(operand2.toString());
+        if (operand3 == null) return s.toString();
+        s.append(",\t");
+        s.append(operand3.toString());
+        if (operand4 == null) return s.toString();
+        s.append(",\t");
+        s.append(operand4.toString());
         return s.toString();
     }
 }
