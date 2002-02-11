4b0d3ad5ba4b56b733a7137ca82fd2b15a9630b6
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-02-11 08:24:26 +0000

    Added compiler passes to the code cache.

--- trunk/joeq_core/joeq/Compiler/Quad/CodeCache.java
+++ trunk/joeq_core/joeq/Compiler/Quad/CodeCache.java
@@ -17,18 +17,27 @@ public class CodeCache {
     public static final CodeCache cache = new CodeCache();
 
     protected java.util.HashMap map = new java.util.HashMap();
+
+    public static java.util.List/*<ControlFlowGraphVisitor>*/ passes = new java.util.LinkedList();
     
     /** Creates new CodeCache */
     public CodeCache() { }
 
     public static ControlFlowGraph getCode(jq_Method m) { return cache._get(m); }
+
+    public static boolean TRACE = false;
     
     protected ControlFlowGraph _get(jq_Method m) {
         ControlFlowGraph cfg = (ControlFlowGraph)map.get(m);
         if (cfg == null) {
+	    if (TRACE) System.out.println("Generating quads for "+m);
             BytecodeToQuad b2q = new BytecodeToQuad(m);
             cfg = b2q.convert();
             map.put(m, cfg);
+            for (java.util.Iterator i = passes.iterator(); i.hasNext(); ) {
+                ControlFlowGraphVisitor v = (ControlFlowGraphVisitor)i.next();
+                v.visitCFG(cfg);
+            }
         }
         return cfg;
     }
