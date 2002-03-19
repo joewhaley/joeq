788e9b10c61b226eebf520fdf96048cf7a1a8cf2
joewhaley <joewhaley@users.noreply.github.com>
2002-03-19 00:46:44 +0000

    *** empty log message ***

---
+++ joeq_core/joeq/Compiler/Quad/PrintCFG.java
@@ -0,0 +1,27 @@
+/*
+ * PrintCFG.java
+ *
+ * Created on March 16, 2002, 12:16 PM
+ */
+
+package Compil3r.Quad;
+
+/**
+ *
+ * @author  John Whaley
+ * @version 
+ */
+public class PrintCFG implements ControlFlowGraphVisitor {
+
+    public java.io.PrintStream out = System.out;
+    
+    /** Creates new PrintCFG */
+    public PrintCFG() {}
+
+    /** Sets output stream. */
+    public void setOut(java.io.PrintStream out) { this.out = out; }
+    
+    /** Prints full dump of the given CFG to the output stream. */
+    public void visitCFG(ControlFlowGraph cfg) { out.println(cfg.fullDump()); }
+    
+}
