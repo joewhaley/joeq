930040fde75ef22a95c51fdf1db1fca0b9536d6f
joewhaley <joewhaley@users.noreply.github.com>
2002-07-08 19:58:51 +0000

    Added missing native method.

---
+++ joeq_native/joeq/ClassLib/ibm13_linux/java/lang/ref/Finalizer.java
@@ -0,0 +1,16 @@
+/*
+ * Finalizer.java
+ *
+ * Created on February 26, 2001, 9:01 PM
+ *
+ * @author  John Whaley
+ * @version 
+ */
+
+package ClassLib.ibm13_linux.java.lang.ref;
+
+public abstract class Finalizer {
+
+    private static void initForReset() { }
+
+}
