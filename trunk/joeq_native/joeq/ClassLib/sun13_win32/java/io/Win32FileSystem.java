3f25e0461e25f724383123088f375a3b51d33171
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-08-06 21:45:03 +0000

    getDriveDirectory is static in 1.3 and virtual in 1.4.

---
+++ trunk/joeq_native/joeq/ClassLib/sun13_win32/java/io/Win32FileSystem.java
@@ -0,0 +1,29 @@
+/*
+ * Win32FileSystem.java
+ *
+ * Created on January 29, 2001, 2:29 PM
+ *
+ * @author  John Whaley
+ * @version 
+ */
+
+package ClassLib.sun13_win32.java.io;
+
+import Run_Time.SystemInterface;
+import Run_Time.Unsafe;
+import jq;
+
+public abstract class Win32FileSystem {
+
+    // gets the current directory on the named drive.
+    private static String getDriveDirectory(int i) {
+        byte[] b = new byte[256];
+        int result = SystemInterface.fs_getdcwd(i, b);
+        if (result == 0) throw new InternalError();
+        String res = SystemInterface.fromCString(Unsafe.addressOf(b));
+        // skip "C:"
+        if (res.charAt(1) == ':') return res.substring(2);
+        else return res;
+    }
+
+}
