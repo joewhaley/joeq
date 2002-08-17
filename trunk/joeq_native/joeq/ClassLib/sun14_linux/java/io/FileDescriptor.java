5c0bcbb3ee79a662662ad3dee5e98c6ae2728ba1
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-08-17 01:11:49 +0000

    *** empty log message ***

---
+++ trunk/joeq_native/joeq/ClassLib/sun14_linux/java/io/FileDescriptor.java
@@ -0,0 +1,14 @@
+/*
+ * FileDescriptor.java
+ *
+ * Created on January 29, 2001, 1:33 PM
+ *
+ * @author  John Whaley
+ * @version 
+ */
+
+package ClassLib.sun14_linux.java.io;
+
+abstract class FileDescriptor {
+    int fd;
+}
---
+++ trunk/joeq_native/joeq/ClassLib/sun14_linux/java/lang/Class.java
@@ -0,0 +1,44 @@
+package ClassLib.sun14_linux.java.lang;
+
+public final class Class {
+    
+    private native java.lang.reflect.Field[] getFields0(int m);
+    private native java.lang.reflect.Method[] getMethods0(int m);
+    private native java.lang.reflect.Constructor[] getConstructors0(int m);
+    
+    private java.lang.reflect.Field[] getDeclaredFields0(boolean publicOnly) {
+	java.lang.reflect.Field[] f = getFields0(java.lang.reflect.Member.DECLARED);
+	if (publicOnly) {
+	    int count = 0;
+	    for (int i=0; i<f.length; ++i) {
+		if (java.lang.reflect.Modifier.isPublic(f[i].getModifiers()))
+		    ++count;
+	    }
+	    java.lang.reflect.Field[] f2 = new java.lang.reflect.Field[count];
+	    --count;
+	    for (int i=0, j=-1; j<count; ++i) {
+		if (java.lang.reflect.Modifier.isPublic(f[i].getModifiers()))
+		    f2[++j] = f[i];
+	    }
+	    f = f2;
+	}
+	return f;
+    }
+    private java.lang.reflect.Method[] getDeclaredMethods0(boolean publicOnly) {
+	java.lang.reflect.Method[] f = getMethods0(java.lang.reflect.Member.DECLARED);
+	if (publicOnly) {
+	}
+	return f;
+    }
+    private java.lang.reflect.Constructor[] getDeclaredConstructors0(boolean publicOnly) {
+	java.lang.reflect.Constructor[] f = getConstructors0(java.lang.reflect.Member.DECLARED);
+	if (publicOnly) {
+	}
+	return f;
+    }
+    
+    private static boolean desiredAssertionStatus0(Class clazz) {
+	// TODO.
+	return false;
+    }
+}
