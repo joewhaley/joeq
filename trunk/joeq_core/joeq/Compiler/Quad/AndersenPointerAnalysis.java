47b0595088d43b5f65b163c4aa2c8eee932b73f4
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-09-06 23:22:54 +0000

    Turn on verify assertions. Clear global node and other nodes.

--- trunk/joeq_core/joeq/Compiler/Quad/AndersenPointerAnalysis.java
+++ trunk/joeq_core/joeq/Compiler/Quad/AndersenPointerAnalysis.java
@@ -45,7 +45,7 @@ public class AndersenPointerAnalysis {
     public static /*final*/ boolean TRACE = false;
     public static final boolean TRACE_CHANGE = false;
     public static final boolean TRACE_CYCLES = false;
-    public static final boolean VerifyAssertions = false;
+    public static final boolean VerifyAssertions = true;
     public static boolean FULL_DUMP = false;
     public static boolean COMPARE_RTA = false;
     public static boolean DO_TWICE = false;
