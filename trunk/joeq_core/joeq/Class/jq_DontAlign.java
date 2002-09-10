fd67705aac4dbf643484c30f47b814a8eed0ac13
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-09-10 22:08:56 +0000

    Add some javadoc.

--- trunk/joeq_core/joeq/Class/jq_DontAlign.java
+++ trunk/joeq_core/joeq/Class/jq_DontAlign.java
@@ -9,7 +9,13 @@ package Clazz;
 
 import Bootstrap.PrimordialClassLoader;
 
-/*
+/**
+ * This interface is used as a marker to signify that the fields in the
+ * class should not be aligned.  This is necessary if the layout must
+ * match another data structure exactly, for example: in
+ * Scheduler.jq_RegisterState.
+ * 
+ * @see Scheduler.jq_RegisterState
  * @author  John Whaley
  * @version $Id$
  */
