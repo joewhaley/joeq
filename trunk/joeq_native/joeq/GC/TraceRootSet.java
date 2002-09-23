638e60a01ddc2dd93712edb790cb7452b98932db
laudney <laudney@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-09-23 15:11:56 +0000

    Framework for tracing gc

---
+++ trunk/joeq_native/joeq/GC/TraceRootSet.java
@@ -0,0 +1,20 @@
+/**
+ * TraceRootSet
+ *
+ * starting points for tracing algorithm used
+ * by Mark-and-Sweep garbage collectors,
+ * which include thread stacks, statics, interned
+ * strings and JNI references.
+ *
+ * Created on Sep 23, 2002, 9:52:59 PM
+ *
+ * @author laudney
+ * @version 0.1
+ */
+package GC;
+
+public class TraceRootSet {
+
+    public static /*final*/ boolean TRACE = false;
+
+}
