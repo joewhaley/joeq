16456abc514059a1e989181d12bd39e664fdcec8
joewhaley <joewhaley@users.noreply.github.com>
2002-09-24 02:27:51 +0000

    Fix to make compilable.

--- joeq_native/joeq/GC/TraceGC.java
+++ joeq_native/joeq/GC/TraceGC.java
@@ -4,7 +4,7 @@
  * Created on Sep 23, 2002, 11:04:51 PM
  *
  * @author laudney <bin_ren@myrealbox.com>
- * @version 0.1
+ * @version $Id$
  */
 package GC;
 
@@ -12,11 +12,11 @@ public abstract class TraceGC {
 
     public static /*final*/ boolean TRACE = false;
 
-    public abstract boolean doWork() {}
+    public abstract boolean doWork();
 
-    public abstract boolean mark() {}
+    public abstract boolean mark();
 
-    public abstract boolean sweep() {}
+    public abstract boolean sweep();
 
-    public abstract boolean compact() {}
+    public abstract boolean compact();
 }
