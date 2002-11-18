7860e4d0c71f755e83c0d75271db7d0233b0dc59
asharm2 <asharm2@1d7dd736-6415-0410-97e1-dd8997bb2113>
2002-11-18 00:08:31 +0000

    *** empty log message ***

---
+++ joeq_native/joeq/ClassLib/Common/java/lang/SecurityManager.java
@@ -0,0 +1,41 @@
+/*
+ * SecurityManager.java
+ *
+ * Created on Novemeber 7, 2002, 3:00 PM
+ *
+ */
+
+package ClassLib.Common.java.lang;
+
+import Clazz.jq_CompiledCode;
+import Main.jq;
+import Memory.CodeAddress;
+import Memory.StackAddress;
+import Run_Time.Reflection;
+import Run_Time.StackCodeWalker;
+import Run_Time.Unsafe;
+
+/**
+ * @author  John Whaley
+ * @version $Id$
+ */
+public class SecurityManager {
+
+    protected java.lang.Class[] getClassContext() {
+        StackCodeWalker sw = new StackCodeWalker(null, StackAddress.getBasePointer());
+        sw.gotoNext();
+        int i;
+        for (i=0; sw.hasNext(); ++i, sw.gotoNext()) ;
+        java.lang.Class[] classes = new java.lang.Class[i];
+        sw = new StackCodeWalker(null, StackAddress.getBasePointer());
+        sw.gotoNext();
+        for (i=0; sw.hasNext(); ++i, sw.gotoNext()) {
+            jq_CompiledCode cc = sw.getCode();
+            if (cc == null) classes[i] = null;
+            else classes[i] = Reflection.getJDKType(cc.getMethod().getDeclaringClass());
+        }
+        jq.Assert(i == classes.length);
+        return classes;
+    }
+
+}
