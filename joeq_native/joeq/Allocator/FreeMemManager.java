159f0a05e52d642043d263db61a33be838ef0fd1
Larry Ren <ren@reddcoin.com>
2002-11-25 14:16:24 +0000

    Added new class FreeMemManager under Allocator to make use of left free memory in every memory block.

---
+++ joeq_native/joeq/Allocator/FreeMemManager.java
@@ -0,0 +1,15 @@
+/**
+ * FreeMemManager
+ *
+ * Created on Nov 26, 2002, 10:07:32 PM
+ *
+ * @author laudney <bin_ren@myrealbox.com>
+ * @version 0.1
+ */
+package Allocator;
+
+import java.util.SortedSet;
+
+public class FreeMemManager {
+    private SortedSet freePool =
+}
