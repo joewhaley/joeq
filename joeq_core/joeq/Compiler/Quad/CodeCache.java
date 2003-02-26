00762cb6d9d03e03493ed9949bd16a411fa63bf6
Michael C. Martin <michaelcmartin@users.noreply.github.com>
2003-02-26 22:09:31 +0000

    Extended Main/Helper support, and easier quad->bytecode mapping

--- joeq_core/joeq/Compiler/Quad/CodeCache.java
+++ joeq_core/joeq/Compiler/Quad/CodeCache.java
@@ -17,15 +17,19 @@ public class CodeCache {
     public static final CodeCache cache = new CodeCache();
 
     protected java.util.HashMap map = new java.util.HashMap();
+    protected java.util.HashMap bcmap = new java.util.HashMap();
 
     public static java.util.List/*<ControlFlowGraphVisitor>*/ passes = new java.util.LinkedList();
+    
     
     /** Creates new CodeCache */
     public CodeCache() { }
 
     public static ControlFlowGraph getCode(jq_Method m) { return cache._get(m); }
+    public static java.util.Map getBCMap(jq_Method m) { return cache._getmap(m); }
 
     public static boolean TRACE = false;
+    public static boolean AlwaysMap = false;
     
     protected ControlFlowGraph _get(jq_Method m) {
         ControlFlowGraph cfg = (ControlFlowGraph)map.get(m);
@@ -34,12 +38,28 @@ public class CodeCache {
             BytecodeToQuad b2q = new BytecodeToQuad(m);
             cfg = b2q.convert();
             map.put(m, cfg);
+	    if (AlwaysMap) {
+		bcmap.put(m, b2q.getQuadToBytecodeMap());
+	    }
             for (java.util.Iterator i = passes.iterator(); i.hasNext(); ) {
                 ControlFlowGraphVisitor v = (ControlFlowGraphVisitor)i.next();
                 v.visitCFG(cfg);
             }
         }
         return cfg;
+    }
+
+    protected java.util.Map _getmap(jq_Method m) {
+        java.util.Map result = (java.util.Map)bcmap.get(m);
+        if (result == null) {
+            if (TRACE) System.out.println("Generating quads for "+m);
+            BytecodeToQuad b2q = new BytecodeToQuad(m);
+            ControlFlowGraph cfg = b2q.convert();
+            map.put(m, cfg);
+	    result = b2q.getQuadToBytecodeMap();
+	    bcmap.put(m, result);
+	}
+        return result;
     }
     
 }
