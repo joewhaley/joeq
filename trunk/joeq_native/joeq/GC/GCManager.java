cefc188407f5fa842feac83b6ad49a6fac8c74e0
laudney <laudney@1d7dd736-6415-0410-97e1-dd8997bb2113>
2003-03-17 10:49:43 +0000

    some enhancement

--- trunk/joeq_native/joeq/GC/GCManager.java
+++ trunk/joeq_native/joeq/GC/GCManager.java
@@ -11,34 +11,32 @@ package GC;
 import java.util.Hashtable;
 
 public class GCManager {
+    // Reference Counting GC
+    public static final int RC_GC = 1;
     // Tracing GC
-    public static final int TR_GC = 0x10000000;
-    // Reference-counting GC
-    public static final int RC_GC = 0x20000000;
-    // Mark & Sweep Tracing GC
-    public static final int MS_TR_GC = 0x10000001;
-    // Copying Tracing GC
-    public static final int CP_TR_GC = 0x10000002;
-    // Incremental Tracing GC
-    public static final int IC_TR_GC = 0x10000004;
-    // Conservative Tracing GC
-    public static final int CS_TR_GC = 0x10000008;
-    //Generational Tracing GC
-    public static final int GN_TR_GC = 0x10000010;
-    // Deferred Reference Counting GC
-    public static final int DF_RC_GC = 0x20000001;
-    // One-bit Reference Counting GC
-    public static final int OB_RC_GC = 0x20000002;
-    // Weighted Refernece Counting GC
-    public static final int WT_RC_GC = 0x20000004;
+    public static final int TR_GC = 2;
+    // Mark and Sweep GC
+    public static final int MS_GC = 4;
+    // Copying GC
+    public static final int CP_GC = 8;
+    // Conservative GC
+    public static final int CNS_GC = 0x10;
+    // Incremental GC
+    public static final int INC_GC = 0x12;
+    // Generational GC
+    public static final int GEN_GC = 0x14;
+    // Parallel GC
+    public static final int PAL_GC = 0x10000;
+    // Distributed GC
+    public static final int DST_GC = 0x20000;
 
     private static boolean initialized = false;
     private static Hashtable candidates = new Hashtable();
-    private static Runnable defaultTracingGC = new TraceMSGC();
+    private static Runnable defaultTCGC = new TraceMSGC();
     private static Runnable defaultRCGC = new SimpleRCGC();
 
     public static void initialize() {
-        candidates.put(new Integer(MS_TR_GC), new TraceMSGC());
+        candidates.put(new Integer(TR_GC & MS_GC), new TraceMSGC());
         candidates.put(new Integer(RC_GC), new SimpleRCGC());
         initialized = true;
     }
@@ -55,7 +53,7 @@ public class GCManager {
         if (result != null) {
             return result;
         } else if ((key & TR_GC) != 0) {
-            return defaultTracingGC;
+            return defaultTCGC;
         } else if ((key & RC_GC) != 0) {
             return defaultRCGC;
         } else {
