5992f2ffb29c0740533c2ff2a9e42077122a98ad
joewhaley <joewhaley@users.noreply.github.com>
2003-04-23 03:06:22 +0000

    Remove stale import.

--- joeq_core/joeq/Class/PrimordialClassLoader.java
+++ joeq_core/joeq/Class/PrimordialClassLoader.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
