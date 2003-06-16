6a080689ddaa5f965bb4aa6e57025f5aeac51a97
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2003-06-16 17:17:50 +0000

    Index map.

---
+++ trunk/joeq_core/joeq/Util/Collections/IndexMap.java
@@ -0,0 +1,52 @@
+// IndexMap.java, created Jun 15, 2003 2:04:05 AM by joewhaley
+// Copyright (C) 2003 John Whaley <jwhaley@alum.mit.edu>
+// Licensed under the terms of the GNU LGPL; see COPYING for details.
+package Util.Collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * IndexMap
+ * 
+ * @author John Whaley
+ * @version $Id$
+ */
+public class IndexMap {
+    private final String name;
+    private final HashMap hash;
+    private final ArrayList list;
+        
+    public IndexMap(String name) {
+        this.name = name;
+        hash = new HashMap();
+        list = new ArrayList();
+    }
+        
+    public int get(Object o) {
+        Integer i = (Integer) hash.get(o);
+        if (i == null) {
+            hash.put(o, i = new Integer(list.size()));
+            list.add(o);
+            if (false) System.out.println(this+"["+i+"] = "+o);
+        }
+        return i.intValue();
+    }
+        
+    public Object get(int i) {
+        return list.get(i);
+    }
+        
+    public boolean contains(Object o) {
+        return hash.containsKey(o);
+    }
+        
+    public int size() {
+        return list.size();
+    }
+        
+    public String toString() {
+        return name;
+    }
+        
+}
