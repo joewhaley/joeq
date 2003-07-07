618a832f6f3a925c709c2dec0ef801274b725a94
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2003-07-07 10:37:19 +0000

    Simple test.

---
+++ trunk/joeq_test/simple/SystemOut.java
@@ -0,0 +1,19 @@
+// SystemOut.java, created Jul 6, 2003 2:30:14 PM by John Whaley
+// Copyright (C) 2003 John Whaley
+// Licensed under the terms of the GNU LGPL; see COPYING for details.
+package simple;
+
+/**
+ * SystemOut
+ * 
+ * @author John Whaley
+ * @version $Id$
+ */
+public class SystemOut {
+
+    public static void main(String[] args) {
+        System.out.println("System.in = "+System.in);
+        System.out.println("System.out = "+System.out);
+        System.out.println("System.err = "+System.err);
+    }
+}
