2fb2a7a6db1dc6662b21ce047bf5f301d21f697c
joewhaley <joewhaley@users.noreply.github.com>
2003-10-11 20:33:02 +0000

    Add object traverser.

--- joeq_native/joeq/ClassLib/apple13_osx/Interface.java
+++ joeq_native/joeq/ClassLib/apple13_osx/Interface.java
@@ -3,7 +3,12 @@
 // Licensed under the terms of the GNU LGPL; see COPYING for details.
 package ClassLib.apple13_osx;
 
+import Bootstrap.ObjectTraverser;
+import Bootstrap.PrimordialClassLoader;
 import ClassLib.ClassLibInterface;
+import Clazz.jq_Class;
+import Run_Time.SystemInterface;
+import UTF.Utf8;
 
 /**
  * Interface
@@ -23,6 +28,37 @@ public final class Interface extends ClassLib.Common.InterfaceImpl {
                                             java.util.Collections.singleton(u).iterator());
         }
         return super.getImplementationClassDescs(desc);
+    }
+
+    public ObjectTraverser getObjectTraverser() {
+        return apple13_osxObjectTraverser.INSTANCE;
+    }
+    
+    public static class apple13_osxObjectTraverser extends CommonObjectTraverser {
+        public static apple13_osxObjectTraverser INSTANCE = new apple13_osxObjectTraverser();
+        protected apple13_osxObjectTraverser() {}
+        public java.lang.Object mapInstanceField(java.lang.Object o, Clazz.jq_InstanceField f) {
+            if (IGNORE_THREAD_LOCALS) {
+                jq_Class c = f.getDeclaringClass();
+                if (c == PrimordialClassLoader.getJavaLangThread()) {
+                    String fieldName = f.getName().toString();
+                    if (fieldName.equals("threadLocals") || fieldName.equals("inheritableThreadLocals")) {
+                        // 1.3.1_05 and greater uses the 1.4 implementation of thread locals.
+                        // see Sun BugParade id#4667411.
+                        // we check the type of the field and return the appropriate object.
+                        if (f.getType().getDesc() == Utf8.get("Ljava/util/Map;")) {
+                            return java.util.Collections.EMPTY_MAP;
+                        } else if (f.getType().getDesc() == Utf8.get("Ljava/lang/ThreadLocal$ThreadLocalMap;")) {
+                            return null;
+                        } else {
+                            SystemInterface.debugwriteln("Unknown type for field "+f);
+                            return null;
+                        }
+                    }
+                }
+            }
+            return super.mapInstanceField(o, f);
+        }
     }
     
 }
