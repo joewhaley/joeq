037d888d0e0835698e8411772031b7c35a153792
joewhaley <joewhaley@users.noreply.github.com>
2003-10-30 06:15:20 +0000

    Thread test.

---
+++ joeq_test/PointerAnalysis/Threads.java
@@ -0,0 +1,41 @@
+// Threads.java, created Oct 29, 2003 8:07:46 AM by joewhaley
+// Copyright (C) 2003 John Whaley <jwhaley@alum.mit.edu>
+// Licensed under the terms of the GNU LGPL; see COPYING for details.
+package PointerAnalysis;
+
+/**
+ * Threads
+ * 
+ * @author John Whaley
+ * @version $Id$
+ */
+public class Threads {
+    
+    public static class ThreadA extends Thread {
+        Threads a, b;
+        ThreadA(Threads a) {
+            this.a = a;
+        }
+        public void run() {
+            b = new Threads();
+            for (;;) {
+                a.foo();
+                b.foo();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Threads a = new Threads();
+        
+        ThreadA t1 = new ThreadA(a);
+        ThreadA t2 = new ThreadA(a);
+        
+        t1.start(); t2.start();
+    }
+    
+    int counter;
+    synchronized void foo() {
+        ++counter;
+    }
+}
