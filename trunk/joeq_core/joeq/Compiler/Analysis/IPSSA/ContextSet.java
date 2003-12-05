fe4c24d5717eeb2db6308df8bcabf989f377e594
cs343 <cs343@1d7dd736-6415-0410-97e1-dd8997bb2113>
2003-12-05 23:02:01 +0000

    Moved it from IPA, since it now depends to SSALocations.

---
+++ trunk/joeq_core/joeq/Compiler/Analysis/IPSSA/ContextSet.java
@@ -0,0 +1,16 @@
+package Compil3r.Analysis.IPSSA;
+
+import Compil3r.Analysis.IPSSA.SSALocation;
+
+public class ContextSet {
+	// TODO: fill in the details of the representation
+	
+	public class ContextLocationPair {
+		protected SSALocation _location; 
+		protected ContextSet  _context;
+	
+		public SSALocation getLocation(){return _location;}
+		public ContextSet  getContext(){return _context;}
+	}
+}
+
