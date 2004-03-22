d40af05070fed6a869c13ce1bf830a0c6ea6935b
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2004-03-22 22:43:23 +0000

    Reverse graph.

---
+++ trunk/joeq_core/joeq/Util/Graphs/ReverseGraph.java
@@ -0,0 +1,42 @@
+// ReverseGraph.java, created Mar 22, 2004 1:57:53 PM 2004 by jwhaley
+// Copyright (C) 2004 John Whaley <jwhaley@alum.mit.edu>
+// Licensed under the terms of the GNU LGPL; see COPYING for details.
+package joeq.Util.Graphs;
+
+import java.util.Collection;
+
+/**
+ * ReverseGraph
+ * 
+ * @author jwhaley
+ * @version $Id$
+ */
+public class ReverseGraph implements Graph {
+    
+    public ReverseGraph(Graph g, Collection newRoots) {
+        _graph = g;
+        _nav = new ReverseNavigator(g.getNavigator());
+        _roots = newRoots;
+    }
+    
+    private Graph _graph;
+    private Navigator _nav;
+    private Collection _roots;
+    
+    /* (non-Javadoc)
+     * @see joeq.Util.Graphs.Graph#getRoots()
+     */
+    public Collection getRoots() {
+        return _roots;
+    }
+    
+    /* (non-Javadoc)
+     * @see joeq.Util.Graphs.Graph#getNavigator()
+     */
+    public Navigator getNavigator() {
+        return _nav;
+    }
+    
+    public Graph getGraph() { return _graph; }
+    
+}
