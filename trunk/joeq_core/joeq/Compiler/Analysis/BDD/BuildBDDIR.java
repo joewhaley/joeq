12473d995eb848d80dc16840f0b3b89aa2285c48
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2004-03-24 07:49:27 +0000

    *** empty log message ***

--- trunk/joeq_core/joeq/Compiler/Analysis/BDD/BuildBDDIR.java
+++ trunk/joeq_core/joeq/Compiler/Analysis/BDD/BuildBDDIR.java
@@ -81,7 +81,7 @@ public class BuildBDDIR extends QuadVisitor.EmptyVisitor implements ControlFlowG
             int index = varOrderDesc.indexOf("xtargetxfallthrough");
             varOrderDesc = varOrderDesc.substring(0, index) + varOrderDesc.substring(index + "xtargetxfallthrough".length());
             
-            varOrderDesc = "memberxquad_method_constant_opc_srcs_dest_srcNum";
+            varOrderDesc = "method_memberxquad_constant_opc_srcs_dest_srcNum";
         }
         if (USE_SRC12) {
             int index = varOrderDesc.indexOf("_srcs");
