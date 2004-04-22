4097ef5ab99a928af4d60b38165dc7b314df723e
joewhaley <joewhaley@users.noreply.github.com>
2004-04-22 00:56:15 +0000

    Added comments.

--- joeq_core/joeq/Util/IO/SystemProperties.java
+++ joeq_core/joeq/Util/IO/SystemProperties.java
@@ -1,10 +1,16 @@
+//SystemProperties.java, created Sun Dec  7 14:20:28 PST 2003
+//Copyright (C) 2004 Godmar Back <gback@cs.utah.edu, @stanford.edu>
+//Licensed under the terms of the GNU LGPL; see COPYING for details.
 package joeq.Util.IO;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 /**
  * Read system properties from a file.
+ * @version $Id$
+ * @author gback
  */
 public class SystemProperties {
     public static void read(String filename) {
