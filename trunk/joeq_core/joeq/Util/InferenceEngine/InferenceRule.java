053f1d49a2f82049011f0b8df759137e66ab87db
joewhaley <joewhaley@1d7dd736-6415-0410-97e1-dd8997bb2113>
2004-04-28 01:59:34 +0000

    *** empty log message ***

--- trunk/joeq_core/joeq/Util/InferenceEngine/InferenceRule.java
+++ trunk/joeq_core/joeq/Util/InferenceEngine/InferenceRule.java
@@ -163,6 +163,8 @@ public abstract class InferenceRule {
                 }
                 neededVariables.put(v, d);
                 String o2 = (String) variableOptions.get(v);
+                if (o.equals("")) o = o2;
+                else if (o2.equals("")) o2 = o;
                 if (o2 != null && !o.equals(o2)) {
                     throw new IllegalArgumentException(v+": "+o+" != "+o2);
                 }
