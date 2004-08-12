a2997c03d0e03e10e1b3e847b9dcac44a082d0d9
joewhaley <joewhaley@users.noreply.github.com>
2004-08-12 02:11:41 +0000

    *** empty log message ***

--- joeq_native/joeq/Scheduler/jq_NativeThread.java
+++ joeq_native/joeq/Scheduler/jq_NativeThread.java
@@ -410,6 +410,16 @@ public class jq_NativeThread implements jq_DontAlign {
     /** Thread switch based on explicit yield. */
     public void yieldCurrentThread() {
         jq_Thread t1 = this.currentThread;
+        if (t1 == this.schedulerThread) {
+            // The scheduler thread is trying to yield.
+            // This occurs when there is contention on a monitor that the scheduler
+            // thread uses, e.g. the monitor for the transfer queue.
+            // The scheduler thread doesn't have anyone to yield to, so we just return,
+            // which will cause us to busy-wait.
+            t1.enableThreadSwitch();
+            Assert._assert(!t1.isThreadSwitchEnabled());
+            return;
+        }
         t1.wasPreempted = false;
         if (TRACE) SystemInterface.debugwriteln("Explicit yield in native thread: " + this + " Java thread: " + t1);
         switchThread();
