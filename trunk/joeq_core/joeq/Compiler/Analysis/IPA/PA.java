4d41b59dd2d5e7e00237ede7b22851e3a9d78b42
livshits <livshits@1d7dd736-6415-0410-97e1-dd8997bb2113>
2006-03-04 01:26:15 +0000

    Enter inlined sites into mI

--- trunk/joeq_core/joeq/Compiler/Analysis/IPA/PA.java
+++ trunk/joeq_core/joeq/Compiler/Analysis/IPA/PA.java
@@ -5518,7 +5518,7 @@ public class PA {
             int c_i = Imap.get(callLoc);
             jq_Method target = Invoke.getMethod(callSite).getMethod();
             addToIE(I.ithVar(c_i), target);
-            //addToMI(M.ithVar(Mmap.get(target)), I.ithVar(c_i), target);
+            addToMI(M.ithVar(Mmap.get(target)), I.ithVar(c_i), target);
             BDD retBDD = Iret.and(I.ithVar(c_i));
             System.out.println("Size of Iret " + Iret.satCount(Iset.and(V1set)));            
             //BigInteger i = retBDD.scanVar(V1);
