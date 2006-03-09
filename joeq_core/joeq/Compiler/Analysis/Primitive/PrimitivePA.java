cb46880590c53671c14d4015bd48e0b95a793d36
Michael C. Martin <michaelcmartin@users.noreply.github.com>
2006-03-09 01:10:16 +0000

    When dumping unmunged methods, dump unmunged names too

--- joeq_core/joeq/Compiler/Analysis/Primitive/PrimitivePA.java
+++ joeq_core/joeq/Compiler/Analysis/Primitive/PrimitivePA.java
@@ -5123,6 +5123,21 @@ public class PrimitivePA {
             } finally {
                 if (dos != null) dos.close();
             }
+            dos = null;
+            try {
+                dos = new BufferedWriter(new FileWriter(dumpPath+"unmunged_name.map"));
+                for (int j = 0; j < Nmap.size(); ++j) {
+                    Object o = Nmap.get(j);
+                    if (o instanceof Dummy) {
+                        dos.write(o.toString()+"\n");
+                        continue;
+                    }
+                    jq_Method m = (jq_Method) o;
+                    dos.write(NameMunger.getJavadocSignature(m) + "\n");
+                }
+            } finally {
+                if (dos != null) dos.close();
+            }        
         }
 
     }
